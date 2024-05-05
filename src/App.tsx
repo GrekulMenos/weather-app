27d98e7a31e6acd7aba7c46484a1e5ea850caf55
Miron <myroslavgrekul@gmail.com>
2024-05-05 16:49:29 +0300

    add clear input onSeact and fetch data on button

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
-import { ChangeEvent, useState } from "react";
+import { ChangeEvent, useEffect, useState } from "react";
+import { optionType } from "./types";
 
 const App = (): JSX.Element => {
   const [input, setInput] = useState<string>("");
+  const [city, setCity] = useState<optionType | null>(null);
   const [options, setOptions] = useState<[]>([]);
 
   const getSearchOptions = (value: string) => {
@@ -24,6 +26,31 @@ const App = (): JSX.Element => {
     console.log(options);
   };
 
+  const getForecast = (option: optionType) => {
+    fetch(
+      `https://api.openweathermap.org/data/2.5/weather?lat=${option.lat}&lon=${option.lon}&units=metric&appid=6e350a23bfb9c1c35cf6005ee0ee03b6`
+    )
+      .then((res) => res.json())
+      .then((data) => console.log({ data }));
+  };
+
+  const onSubmit = () => {
+    if (!city) return;
+
+    getForecast(city);
+  };
+
+  const onOptionSelect = (option: optionType) => {
+    setCity(option);
+  };
+
+  useEffect(() => {
+    if (city) {
+      setInput(city.name);
+      setOptions([]);
+    }
+  }, [city]);
+
   return (
     <main className="flex justify-center items-center bg-gradient-to-br from-sky-400 via-rose-400 to-lime-400 h-[100vh] w-full">
       <section className="  drop-shadow-lg rounded w-full md:max-w-[500px] p-4 flex flex-col text-center items-center justify-center  md:px-10 lg:p-24 h-full lg:h-[500px] bg-white bg-opacity-20 backdrop-blur-lg   text-zinc-700">
@@ -43,16 +70,22 @@ const App = (): JSX.Element => {
           />
 
           <ul className="absolute top-9 bg-white ml-1 rounded-b-md">
-            {options.map((option: { name: string }, index: number) => (
+            {options.map((option: optionType, index: number) => (
               <li key={option.name + "-" + index}>
-                <button className="cursor-pointer text-left text-sm w-full hover:bg-zinc-700 hover:text-white px-2 py-1">
+                <button
+                  onClick={() => onOptionSelect(option)}
+                  className="cursor-pointer text-left text-sm w-full hover:bg-zinc-700 hover:text-white px-2 py-1"
+                >
                   {option.name}
                 </button>
               </li>
             ))}
           </ul>
 
-          <button className="rounded-r-md border-2 border-zinc-100 hover:border-zinc-500 hover:text-zinc-500 text-zinc-100 px-2 py-1 cursor-pointer">
+          <button
+            onClick={onSubmit}
+            className="rounded-r-md border-2 border-zinc-100 hover:border-zinc-500 hover:text-zinc-500 text-zinc-100 px-2 py-1 cursor-pointer"
+          >
             search
           </button>
         </div>
